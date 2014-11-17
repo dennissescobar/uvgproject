f7252c2ba83a39c1e5324e52ba6b43580dccf77c
dennissescobar <dennissescobar@gmail.com>
2014-11-16 19:37:45 -0600

    redirecciom y fumncion

---
+++ UVGProject/database/dbo.usuario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [nombre] NVARCHAR(150) NOT NULL, 
+    [apellido] NVARCHAR(150) NOT NULL, 
+    [correo] NVARCHAR(150) NOT NULL, 
+    [imagen] NVARCHAR(150) NULL, 
+    [password] NVARCHAR(16) NOT NULL
+)
